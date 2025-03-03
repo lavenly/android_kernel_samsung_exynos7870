7fcb5c6f7025086334d3e6bba96e5943e2549ad8
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-04 00:32:25 +0800

    fs: integrate KernelSU using manual hooks v1.9
    This patch hooks
    - exec: sys_execve (3.18, via do_execve_common)
    - open: sys_faccessat (4.14 and older)
    - stat: sys_newfstatat
    - sys: sys_reboot (3.18+)

--- fs/exec.c
+++ fs/exec.c
@@ -1610,6 +1610,13 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd,
+			struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
+#endif
+
 static int do_execve_common(struct filename *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp)
@@ -1621,6 +1628,10 @@ static int do_execve_common(struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
